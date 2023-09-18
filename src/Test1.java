556eeee75d91be2673667a97915b942406d97160
SerkanCuteli <serkancuteli@gmail.com>
2023-09-18 21:38:22 +0200

    Test1 Updated on My 1905 Branch

--- src/Test1.java
+++ src/Test1.java
@@ -5,5 +5,7 @@ public class Test1 {
         System.out.println("Test1");
         System.out.println("ADDED LOCALLY!!!");
         System.out.println("ADDED LOCALLY, pushed with GIT MENU!!!");
+        // YENI BRANCH DA BU SATIR EKLENDI (GS-1905_Login_Test)
     }
 }
+
