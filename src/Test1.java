28cdaa0646d7da569dc9114c05c9b72863ef2463
SerkanCuteli <145158431+SerkanCuteli@users.noreply.github.com>
2023-09-18 21:26:26 +0200

    Update Test1.java at rmote repo

--- src/Test1.java
+++ src/Test1.java
@@ -5,5 +5,6 @@ public class Test1 {
         System.out.println("Test1");
         System.out.println("ADDED LOCALLY!!!");
         System.out.println("ADDED LOCALLY, pushed with GIT MENU!!!");
+        // Burasi remote repoda github ta eklendi
     }
 }
